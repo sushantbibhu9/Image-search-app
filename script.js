4908df4eadabc0b49e06de2dcb5582939aaf2001
Sushant Singh <sushantbibhu@gmail.com>
2023-06-26 00:20:17 +0530

    some-change

--- script.js
+++ script.js
@@ -10,17 +10,16 @@ let page = 1;
 
 async function searchImages() {
     inputData = inputEl.value;
-    const url = `https://unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
 
     const results = data.results;
 
-    if(page === 1){
-        searchResults.innerHTML = ""
+    if(page === 1) {
+        searchResults.innerHTML = "";
     }
-}
 
 results.map((result) => {
     const imageWrapper = document.createElement("div");
@@ -40,5 +39,15 @@ results.map((result) => {
 
 page++
 if(page > 1){
-    showMore.computedStyleMap.display = "block"
+    showMore.style.display = "block";
+  }
 }
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+showMore.addEventListener("click", () => {
+    searchImages();
+});
